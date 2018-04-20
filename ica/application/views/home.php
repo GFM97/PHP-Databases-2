a37ecb961968f4491756912d70512fb1104d6435
GFM97 <33311109+GFM97@users.noreply.github.com>
2018-04-20 15:36:43 +0200

    Set Up
    - Xampp config
    - Menu

---
+++ ica/application/views/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MCAST ICA</title>
+        <link rel="stylesheet" href="<?=base_url('css/font-awesome.min.css')?>">
+        <link rel="stylesheet" href="<?=base_url('css/style.css')?>">
+    </head>
+    <body>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#studentport">Student Portfolio</a>
+            <a href="#studentlink">Student Links</a>
+            <a href="#vacancies">Vacancies</a>
+            <a href="#canclec">Cancelled Lectures</a>
+            <a href="#notices">Notices</a>
+            <a href="#contact">Contact Us</a>
+        </div>
+    </body>
+</html>
