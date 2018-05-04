2563933d377fa2475b255a90f2bfb4e174376bb5
GFM97 <33311109+GFM97@users.noreply.github.com>
2018-05-04 16:26:03 +0200

    Header, Body, Footer
    - Arranged header, body and footer
    - Arranged controllers

---
+++ ica/application/views/templates/bottom.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <p>Information for footer goes here</p>
+</div>
+
+    </body>
+</html>
---
+++ ica/application/views/templates/top.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MCAST ICA</title>
+        <link rel="stylesheet" href="<?=base_url('css/font-awesome.min.css')?>">
+        <link rel="stylesheet" href="<?=base_url('css/style.css')?>">
+        <link rel="stylesheet" href="<?=base_url('font//GothamMedium')?>">
+    </head>
+    <body>
+        <div id="container">
+            <div class="header">
+                <div class="topnav">
+                    <ul>
+                    <img src="images/solid_normal.png" alt="logo">
+                    <p>Institute for <br> the Creative Arts</p>
+                    <li><a class="active" href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#studentport">Student Portfolio</a></li>
+                    <li><a href="#studentlink">Student Links</a></li>
+                    <li><a href="#vacancies">Vacancies</a></li>
+                    <li><a href="#canclec">Cancelled Lectures</a></li>
+                    <li><a href="#notices">Notices</a></li>
+                    <li><a href="#contact">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
